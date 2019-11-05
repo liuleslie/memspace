08bd169a8c9ffa4cff9c4ff1f342f5aa9d362e2a
liuleslie <54601563+liuleslie@users.noreply.github.com>
2019-11-05 14:02:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+
+
+
+
+// AFRAME.registerComponent('portalshift',{
+// 	init:function(){
+// 		var view=document.getElementById("portal");
+// 		this.el.addEventListener('click',function(){
+// 			view.emit("animation__move=property:position;dur:5000;to:-1.14 -1.08 -3.545;target:#portal1_intorus;delay:3000");
+// 		})
+// 	}
+// })
+
